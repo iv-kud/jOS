5dcba5102ff37936f2f5678d287ad04f3a8a41ef
i.zamyatin <i.zamyatin@integrasources.com>
2025-05-18 19:21:39 +0700

    feat: Added character and string output

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,11 +1,18 @@
+#include <arch/x86/VGADisplay/Display.hpp>
+
 extern "C" void kernel_main() {
-    const char* hello = "Hello JustOS!";
-    char* vide_memory = (char*) 0xB8000;
-    for (int i = 0; hello[i] != '\0'; i++)
-    {
-        vide_memory[i * 2] = hello[i];
-        vide_memory[i * 2 + 1] = 0x0A;
-    }
-    
+    Display display;
+
+    display.printChar('H');
+    display.printChar('e');
+    display.printChar('l');
+    display.printChar('l');
+    display.printChar('o');
+    display.printChar(' ');
+    display.printChar('j');
+    display.printChar('O');
+    display.printChar('S');
+    display.printChar('\n');
+    display.printChars("This is just string");
     while(true);
 }
