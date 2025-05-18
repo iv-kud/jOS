7e1f683c0c6f7967f0ad1f19e14d676cd73cd92d
i.zamyatin <i.zamyatin@integrasources.com>
2025-05-18 19:20:24 +0700

    feat: size validation for data types

---
+++ arch/x86/src/core/types/base.cpp
@@ -0,0 +1,5 @@
+#include <core/types/base.hpp>
+static_assert(sizeof(uint8_t) == 1, "uint8_t must be 1 byte");
+static_assert(sizeof(uint16_t) == 2, "uint8_t must be 2 byte");
+static_assert(sizeof(uint32_t) == 4, "uint32_t must be 4 byte");
+static_assert(sizeof(uint64_t) == 8, "uint64_t must be 8 byte");
