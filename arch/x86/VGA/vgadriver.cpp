88f1c876beedd5d610e74833ca764cc0e4802d45
iv-kud <pwasiliy@bk.ru>
2026-01-09 20:21:26 +0700

    fix: rename file and changed logic

---
+++ arch/x86/VGA/vgadriver.cpp
@@ -0,0 +1,96 @@
+#include "vgadriver.h"
+
+VGADriver::VGADriver ()
+  : m_x(0)
+  , m_y(0)
+  , m_video_memory((uint16_t*)0xb8000)
+  , m_color(CHARS::CHAR_COLOR::COLOR_WHITE)
+{}
+
+VGADriver::VGADriver(const CHARS::CHAR_COLOR color)
+  : m_x(0)
+  , m_y(0)
+  , m_video_memory((uint16_t*)0xb8000)
+  , m_color(color)
+{}
+
+VGADriver &VGADriver::instance()
+{
+  static VGADriver driver;
+  return driver;
+}
+
+uint8_t VGADriver::getX() const
+{
+  return m_x;
+}
+
+uint8_t VGADriver::getY() const
+{
+  return m_y;
+}
+
+void VGADriver::setX(const uint8_t newX)
+{
+  if (m_x != newX) {
+      m_x = newX;
+  }
+}
+
+void VGADriver::setY(const uint8_t newY)
+{
+  if (m_y != newY) {
+      m_y = newY;
+    }
+}
+
+void VGADriver::setColor(const CHARS::CHAR_COLOR color)
+{
+  if (m_color != color) {
+      m_color = color;
+    }
+}
+
+uint8_t VGADriver::getBuffWidth() const
+{
+  return m_width;
+}
+
+uint8_t VGADriver::getBuffHeight() const
+{
+  return m_height;
+}
+
+void VGADriver::newLine()
+{
+  m_x = 0;
+  ++m_y;
+}
+
+void VGADriver::horizontalTab()
+{
+  m_x = nextTabPosition(m_x);
+  if(m_x >= m_width)
+    newLine();
+}
+
+uint8_t VGADriver::nextTabPosition(const uint8_t cur_pos) const
+{
+  uint8_t tab_size = 4;
+  return cur_pos + tab_size - (cur_pos % tab_size);
+}
+
+void VGADriver::print(const char ch)
+{
+  switch (ch)
+  {
+  case (uint8_t)CHARS::ESCAPE_CHARS::N_LINE:newLine();break;
+  case (uint8_t)CHARS::ESCAPE_CHARS::H_TAB: horizontalTab();break;
+  default:
+    uint16_t *charPos = m_video_memory + (m_y * m_width  + m_x);
+    *charPos = ((uint8_t)m_color << (uint8_t)8) | ch;
+    m_x++;
+    break;
+  }
+}
+
---
+++ arch/x86/VGA/vgadriver.h
@@ -0,0 +1,40 @@
+#ifndef VGADRIVER_H
+#define VGADRIVER_H
+
+#include "types/data_types.h"
+#include "chars/chars_code.h"
+#include "chars/vga_modecolor.h"
+
+class VGADriver
+{
+public:
+  static VGADriver& instance();
+
+  uint8_t getX() const;
+  uint8_t getY() const;
+  void setX(const uint8_t newX);
+  void setY(const uint8_t newY);
+  void setColor(const CHARS::CHAR_COLOR color);
+  uint8_t getBuffWidth() const;
+  uint8_t getBuffHeight() const;
+  void newLine();
+  void print(const char ch);
+
+  VGADriver(const VGADriver&) = delete;
+  VGADriver& operator=(const VGADriver&) = delete;
+
+private:
+  VGADriver();
+  VGADriver(const CHARS::CHAR_COLOR color);
+  void horizontalTab();
+  uint8_t nextTabPosition(const uint8_t cur_pos) const;
+
+private:
+  const uint8_t m_width = 80;
+  const uint8_t m_height = 25;
+  uint8_t m_x, m_y;
+  uint16_t* m_video_memory;
+  CHARS::CHAR_COLOR m_color;
+};
+
+#endif // VGADRIVER_H
