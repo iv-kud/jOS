7c39a9fd1efced166ec29174a87e20db298aba0e
i.zamyatin <i.zamyatin@kedrsolutions.ru>
2026-05-08 11:50:59 +0700

    feat: add clang-format for automatic code formatting.

---
+++ clang-format.cmake
@@ -0,0 +1,26 @@
+
+if (ENABLE_CLANG_FORMATTING)
+    find_program(CLANG_FORMAT_EXECUTABLE clang-format REQUIRED)
+
+    file(GLOB_RECURSE ALL_SOURCE_FILES
+            ${CMAKE_SOURCE_DIR}/arch/*.cpp
+            ${CMAKE_SOURCE_DIR}/arch/*.h
+            ${CMAKE_SOURCE_DIR}/kernel/*.hpp
+            ${CMAKE_SOURCE_DIR}/kernel/*.cpp
+            ${CMAKE_SOURCE_DIR}/lib/*.h
+            ${CMAKE_SOURCE_DIR}/lib/*.hpp
+    )
+
+    if (ALL_SOURCE_FILES)
+        add_custom_target(
+                clangformat
+                ALL
+                COMMAND ${CLANG_FORMAT_EXECUTABLE}
+                -style=file
+                -i
+                ${ALL_SOURCE_FILES}
+                COMMENT "Running clang-format"
+                VERBATIM
+        )
+    endif()
+endif()
