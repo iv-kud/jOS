367975c28381adf2e7c276b3fea12a4c81bc5d87
iv-kud <pwasiliy@bk.ru>
2026-05-28 21:29:49 +0700

    feat: initialize keyboard interrupts

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,12 +1,21 @@
+#include "arch/x86/core/portIO/port.h"
 #include "display/display.h"
+#include "drivers/keyboarddriver.h"
 #include "table/GDT/global_descriptor.h"
 #include "table/IDT/interrupt_descriptor.h"
 
 extern "C" void main()
 {
-    jInfo() << "Welcome to jOS: " << 0 << '.' << 0 << '.' << 1;
     GlobalDescriptor gdt;
     InterruptDescriptor idt;
+
+    static KeyboardDriver keyboard;
+    keyboard.init();
+
+    Port::write_port(0x21, 0xFD);
+    Port::write_port(0xA1, 0xFF);
+
+    asm volatile("sti");
 
     while (true)
         ;
