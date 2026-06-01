a11de0a23175cf73399721623a1fbffe3e78766a
iv-kud <pwasiliy@bk.ru>
2026-06-01 22:47:11 +0700

    refactor: replace magic number

--- arch/x86/drivers/keyboarddriver.cpp
+++ arch/x86/drivers/keyboarddriver.cpp
@@ -4,17 +4,20 @@
 
 void KeyboardDriver::init()
 {
-    InterruptHandler::instance().registerHandlers(0x21, this);
+    InterruptHandler::instance().registerHandlers(KEYBOARD_IRQ_VECTOR, this);
 }
 
 void KeyboardDriver::handleInterrupt(Registers reg)
 {
-    uint8_t status = Port::read_port(0x64);
-    if (status & 0x01) {
-        uint8_t scancode = Port::read_port(0x60);
-        if (scancode & 0x80)
-            return;
+    uint8_t status = Port::read_port(KEYBOARD_STATUS_PORT);
 
-        jDebug() << "Keyboard scancode: " << NumberBase::Hex << (uint64_t) scancode;
-    }
+    if (!(status & KEYBOARD_STATUS_OUTPUT_FULL))
+        return;
+
+    uint8_t scancode = Port::read_port(KEYBOARD_DATA_PORT);
+
+    if (scancode & KEYBOARD_SCANCODE_RELEASE)
+        return;
+
+    jDebug() << "Keyboard scancode: " << NumberBase::Hex << static_cast<uint64_t>(scancode);
 }
--- arch/x86/drivers/keyboarddriver.h
+++ arch/x86/drivers/keyboarddriver.h
@@ -7,8 +7,17 @@ class KeyboardDriver : public IRQDriver
 {
 public:
     KeyboardDriver() = default;
+
     void init() override;
     void handleInterrupt(Registers reg) override;
+
+private:
+    static constexpr uint8_t KEYBOARD_DATA_PORT   = 0x60;
+    static constexpr uint8_t KEYBOARD_STATUS_PORT = 0x64;
+    static constexpr uint8_t KEYBOARD_IRQ_VECTOR  = 0x21;
+
+    static constexpr uint8_t KEYBOARD_STATUS_OUTPUT_FULL = 0x01;
+    static constexpr uint8_t KEYBOARD_SCANCODE_RELEASE   = 0x80;
 };
 
 #endif // KEYBOARDDRIVER_H
